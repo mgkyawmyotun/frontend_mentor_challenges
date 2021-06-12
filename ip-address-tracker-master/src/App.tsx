6da821cfc92749d85b2359b1d7b0dadae3576ff0
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-06-12 11:09:24 +0630

    start challenge

---
+++ ip-address-tracker-master/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h1>Bar Lae</h1>
+    </div>
+  );
+}
+
+export default App;
